8001dd592c509ff7c58fdf08cb8d7c980516ac0c
Matthew Dobson <mdobson4@gmail.com>
2016-09-01 10:12:07 -0400

    Added tenant formatting tests.

--- test/test_models.js
+++ test/test_models.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
 var rootFormatSiren = require('../formats/siren/root.siren');
 var rootFormatCsv = require('../formats/csv/root.csv');
+var tenantFormatSiren = require('../formats/siren/tenant.siren');
+var tenantFormatCsv = require('../formats/csv/tenant.csv');
 var data = require('./mocks/mock_root_data').postFormat;
 var csvData = require('./mocks/mock_root_data').csvData;
+var tenantData = require('./mocks/mock_tenant_data').postFormat;
+var tenantCsvData = require('./mocks/mock_tenant_data').csvData;
 
 var env = {
   helpers: {
@@ -50,10 +54,37 @@ describe('root models', function(){
   it('will create a CSV', function(){
     var model = rootFormatCsv({env: env, mappings: data});
     assert.equal(model, csvData);
-
-  })
+  });
 });
 
 describe('tenant models', function(){
+  it('will properly create a tenant object.', function(){
+    var model = tenantFormatSiren({env: env, mappings: tenantData});
+    assert.equal(model.class[0], 'tenant');
+    assert.equal(model.properties.startDate, "2016-08-15T04:00:00.000Z");
+    assert.equal(model.properties.endDate, "2016-08-16T04:00:00.000Z");
+    assert.equal(model.properties.aggregation, "day");
+    assert.equal(model.entities.length, 37);
 
+    var entity = model.entities[0];
+    assert.ok(entity.properties);
+    assert.ok(entity.class);
+    assert.ok(entity.rel);
+  });
+
+  it('will create a properly formatted embedded entity', function(){
+    var model = tenantFormatSiren({env: env, mappings: tenantData});
+
+    var entity = model.entities[0];
+    assert.ok(entity.properties.tenantId, 'default');
+    assert.ok(entity.properties.hubId, '00279aa9-02be-47d8-8d49-809a7f16e81b');
+    assert.ok(entity.properties.data.length, 2);
+    assert.equal(entity.class[0], 'usage');
+    assert.equal(entity.rel[0], 'item');
+  });
+
+  it('will create a CSV', function(){
+    var model = tenantFormatCsv({env: env, mappings: tenantData});
+    assert.equal(model, tenantCsvData);
+  });
 });
