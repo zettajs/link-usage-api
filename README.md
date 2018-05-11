d055a1544ef0b9256014f7569ae9fa9ff09eae06
Adam Magaluk <adammagaluk@google.com>
2018-05-11 16:51:09 -0600

    b/79585331 adding Kokoro config.
    Change-Id: Ia8df162c76b6b99db6716041f602a33db7257738

---
+++ README.md
@@ -0,0 +1,10 @@
+# Link Usage API
+
+API to consume the Link usage data in InfluxDB and 
+generate a HTTP API.
+
+## Testing
+
+```
+npm test
+```
--- kokoro/build.sh
+++ kokoro/build.sh
@@ -14,6 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/sh
+#!/bin/bash
+set -x
+cd git/link-usage-api
 
-docker build -t zetta/zetta-usage-api .
+node -v
+npm -v
+
+npm install
+npm test
